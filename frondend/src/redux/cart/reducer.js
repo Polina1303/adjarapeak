847a1c37aad9a28a9b42042306cec9554b319979
Polina Shpak <uznavenko.polina@mail.ru>
2023-05-01 15:42:38 +0400

    fix

--- frondend/src/redux/cart/reducer.js
+++ frondend/src/redux/cart/reducer.js
@@ -1,8 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
-localStorage.setItem("productInCart", JSON.stringify([]));
+
+const CART_KEY = "productInCart";
+
+localStorage.setItem(CART_KEY, JSON.stringify([]));
 
 const getLocalCartData = () => {
-  const localCartData = localStorage.getItem("productInCart");
+  const localCartData = localStorage.getItem(CART_KEY);
   if (localCartData) {
     return JSON.parse(localCartData);
   } else {
@@ -27,17 +30,14 @@ const cartSlice = createSlice({
           state.itemsInCart.push({ ...action.payload, count: 1 });
         }
       }
-      localStorage.setItem("productInCart", JSON.stringify(state.itemsInCart));
+      localStorage.setItem(CART_KEY, JSON.stringify(state.itemsInCart));
     },
-    deletItemFromCart: (state, action) => {
+    deleteItemFromCart: (state, action) => {
       if (state && state.itemsInCart) {
         state.itemsInCart = state.itemsInCart.filter(
           (item) => item.id !== action.payload
         );
-        localStorage.setItem(
-          "productInCart",
-          JSON.stringify(state.itemsInCart)
-        );
+        localStorage.setItem(CART_KEY, JSON.stringify(state.itemsInCart));
       }
     },
     plusItem: (state, action) => {
@@ -46,10 +46,7 @@ const cartSlice = createSlice({
         state.itemsInCart.find((item) => item.id === action.payload.id);
       if (findItem && findItem.count > 0) {
         findItem.count++;
-        localStorage.setItem(
-          "productInCart",
-          JSON.stringify(state.itemsInCart)
-        );
+        localStorage.setItem(CART_KEY, JSON.stringify(state.itemsInCart));
       }
     },
     minusItem: (state, action) => {
@@ -58,19 +55,20 @@ const cartSlice = createSlice({
         state.itemsInCart.find((item) => item.id === action.payload.id);
       if (findItem && findItem.count > 1) {
         findItem.count--;
-        localStorage.setItem(
-          "productInCart",
-          JSON.stringify(state.itemsInCart)
-        );
+        localStorage.setItem(CART_KEY, JSON.stringify(state.itemsInCart));
       }
     },
   },
 });
 
 window.addEventListener("beforeunload", () => {
-  localStorage.setItem("productInCart", JSON.stringify(getLocalCartData()));
+  localStorage.setItem(CART_KEY, JSON.stringify(getLocalCartData()));
 });
 
-export const { setItemInCart, deletItemFromCart, minusItem, plusItem } =
+window.addEventListener("unload", () => {
+  localStorage.setItem(CART_KEY, JSON.stringify(getLocalCartData()));
+});
+
+export const { setItemInCart, deleteItemFromCart, minusItem, plusItem } =
   cartSlice.actions;
 export default cartSlice.reducer;
