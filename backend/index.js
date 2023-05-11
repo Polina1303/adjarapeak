26ad84fdf431f30844466d91e52b1904db60601d
Polina Shpak <uznavenko.polina@mail.ru>
2023-05-11 18:40:07 +0400

    versel

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,11 @@ dotenv.config();
 import express from "express";
 import cors from "cors";
 import { EmailSender } from "./sendEmail.js";
+
 const app = express();
 app.use(express.json());
 app.use(cors());
+
 const PORT = process.env.PORT || 5001;
 app.post("/send", async (req, res) => {
   try {
