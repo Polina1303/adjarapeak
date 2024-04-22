55b23e56c4b50d4ea47e6d50bbe404211f2ffc09
Polina Shpak <uznavenko.polina@mail.ru>
2024-04-22 10:37:31 +0400

    work time

--- frondend/src/components/header/header.js
+++ frondend/src/components/header/header.js
@@ -24,7 +24,7 @@ export const Header = () => {
           rel="noreferrer"
         >
           Батуми ул.Тбел-Абусеридзе, 38
-          <p className="work">понедельник, среда, пятница (11:00-19:00)</p>
+          <p className="work">вторник-воскресенье (11:00-19:00)</p>
         </a>
       </div>
       <div className="number">
