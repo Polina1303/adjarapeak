1df0c68b57466513e036978e0235483c03743999
Polina <uznavenko.polina@mai.ru>
2024-12-10 20:11:40 +0400

    add

--- frondend/src/components/chooseus/chooseus.js
+++ frondend/src/components/chooseus/chooseus.js
@@ -21,11 +21,11 @@ export const ChooseUs = () => {
         </p>
         <div className="choose-item">
           <AiOutlineCheckCircle size={25} className="choose-checkin" />
-          <h5> {languages==="RU"? 'СКИДКА 15%':"DISCOUNT 15% "} </h5>
+          <h5> {languages==="RU"? 'БОНУС ЗА ПОДПИСКУ':"SUBSCRIPTION BONUS"}  </h5>
         </div>
-        <p className="choose-item-description">
-        {languages==="RU"? 'Всем подписчикам нашего':"To all our followers"}<a href="https://t.me/adjarapeak/229" target="_blank" rel="noreferrer" style={{color:'rgba(0, 136, 204)'}}>{languages==="RU"? 'Telegram-канал':"Telegram-channel"} </a>{languages==="RU"? 'предоставляется СКИДКА 15% на весь прокат снаряжения!':"A 15% DISCOUNT is provided for all equipment rentals!"}  
-
+        <p className="choose-item-description"> 
+        {languages==="RU"? 'Всем подписчикам нашего':"To all our followers"}<a href="https://t.me/adjarapeak/229" target="_blank" rel="noreferrer" style={{color:'rgba(0, 136, 204)'}}>{languages==="RU"? ' Telegram-канал':"Telegram-channel"} </a>{languages==="RU"? 'предоставляется СКИДКА 5% на весь прокат снаряжения! Кроме того, если вы отметите нас в социальных сетях, мы добавим еще 5% кэшбэка от суммы вашего заказа. Чтобы воспользоваться предложением, используйте промокод «ПОДПИСКА» при бронировании на сайте, или покажите подписку на своём телефоне при выдаче снаряжения. Кэшбэк возвращается, если у вас открытый аккаунт и отметка кликабельна.':""}   
+ 
         </p>
         <div className="choose-item">
           <AiOutlineCheckCircle size={25} className="choose-checkin" />
@@ -51,8 +51,7 @@ export const ChooseUs = () => {
           <h5>{languages==="RU"? 'БУДЬТЕ ГОТОВЫ К ИЗМЕНЕНИЯМ':"BE PREPARED FOR CHANGES"}</h5>
         </div>
         <p className="choose-item-description">
-        {languages==="RU"? 'В походе могут возникнуть непредвиденные обстоятельства, поэтому важно быть гибким и адаптироваться к изменениям планов. Умейте принимать решения и реагировать на ситуации.':"Unforeseen circumstances may arise during the campaign, so it is important to be flexible and adapt to changes in plans. Be able to make decisions and react to situations."}
-        
+        {languages==="RU"? 'В походе могут возникнуть непредвиденные обстоятельства, поэтому важно быть гибким и адаптироваться к изменениям планов. Умейте принимать решения и реагировать на ситуации. Планируйте запасной маршрут – изучите альтернативные пути и точки выхода из маршрута на случай чрезвычайной ситуации. Имейте минимальный набор экстренной помощи – аптечку, запас еды и воды, а также средства связи.':"Unforeseen circumstances may arise during the campaign, so it is important to be flexible and adapt to changes in plans. Be able to make decisions and react to situations."}        
         </p>
         <div className="choose-item">
           <FaHandPointUp size={25} className="choose-checkin" />
