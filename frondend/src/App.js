7871e8708a29d3ca4bbd0521767e12258b12bb03
Polina <uznavenko.polina@mai.ru>
2025-07-17 14:49:39 +0400

    add

--- frondend/src/App.js
+++ frondend/src/App.js
@@ -29,6 +29,7 @@ import { RockClimbingPage } from "./pages/rock-climbing-page";
 import { RentSkyPage } from "./pages/rent-sky-page";
 import { ReturnPolicyPage } from "./pages/return-policy-page/return-policy-page";
 import { HikhaniTripPage } from "./pages/hihani-page";
+import { UchkhoTripPage } from "./pages/uchkho-page";
 
 function App() {
   return (
@@ -57,6 +58,7 @@ function App() {
           <Route exact path="/erge" element={<ErgeRoutePage />} />
           <Route exact path="/garden" element={<GardenRoutePage />} />
           <Route exact path="/hikhani" element={<HikhaniTripPage />} />
+          <Route exact path="/uchkho" element={<UchkhoTripPage />} />
           <Route exact path="*" element={<Error />} />
           <Route exact path="/return_policy" element={<ReturnPolicyPage />} />
         </Routes>
