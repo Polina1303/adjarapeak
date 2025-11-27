f58cbaf41499afc5b825b4b4bc017595037a99e0
Polina <uznavenko.polina@mai.ru>
2025-11-27 17:34:33 +0400

    add

--- frondend/src/pages/home-page/home-page.js
+++ frondend/src/pages/home-page/home-page.js
@@ -183,6 +183,64 @@ export const HomePage = () => {
         </article>
       </section>
 
+      <section className="service-section">
+        <h2 className="routes-title">
+          {languages === "RU"
+            ? "Сервисное обслуживание"
+            : "Maintenance service"}
+        </h2>
+        <table className="service-table">
+          <thead>
+            <tr>
+              <th>{languages === "RU" ? "Услуга" : "Service"}</th>
+
+              <th>{languages === "RU" ? "Цена" : "Price"}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                {languages === "RU"
+                  ? "Комплексное обслуживание - заточка кантов, чистка скользящей поверхности, снятие старого парафина, нанесение нового (парафины от +3 до -20, с шагом 6 градусов)"
+                  : "Comprehensive Service - Edge sharpening, base cleaning, old wax removal, and new wax application (wax range from +3°C to -20°C, in 6-degree increments)."}{" "}
+              </td>
+
+              <td>~ 80 {languages === "RU" ? "лари" : "gel"}</td>
+            </tr>
+            <tr>
+              <td>Заточка кантов</td>
+
+              <td>~ 40 {languages === "RU" ? "лари" : "gel"}</td>
+            </tr>
+            <tr>
+              <td>Консервация</td>
+
+              <td>от 20 {languages === "RU" ? "лари" : "gel"}</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <button className="rent-rules-button" onClick={toggleModal}>
+          <h3 className="rent-rules-title">ПОСМОТРЕТЬ ФОТО РАБОТЫ</h3>
+        </button>
+
+        {isOpen && (
+          <div className="modal-overlay" onClick={toggleModal}>
+            <div
+              className="modal-content"
+              onClick={(e) => e.stopPropagation()} // Чтобы клик внутри окна не закрывал его
+            >
+              <button className="close-button" onClick={toggleModal}>
+                ×
+              </button>
+              <div className="images-container">
+                <img src={img2} alt="Image 1" className="modal-image" />
+                <img src={img1} alt="Image 2" className="modal-image" />
+              </div>
+            </div>
+          </div>
+        )}
+      </section>
       <RockClimbing />
       <Routes />
       <RulesPage />
