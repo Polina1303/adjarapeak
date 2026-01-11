fddaa307e47981ffe057d9234f43cc61aa303514
Polina <uznavenko.polina@mai.ru>
2026-01-11 20:39:57 +0400

    fix

--- frondend/pages/index.js
+++ frondend/pages/index.js
@@ -9,24 +9,28 @@
 //     },
 //   };
 // }
-
-import { HomePage } from "../src/pages/home-page";
+import HomePage from "../src/pages/home-page";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 export default HomePage;
 
 export async function getServerSideProps({ locale }) {
+  const config = {
+    i18n: {
+      defaultLocale: "ru",
+      locales: ["ru", "en", "ka"],
+    },
+    ns: ["common", "rent", "sale", "header"],
+    defaultNS: "common",
+  };
+
   return {
     props: {
-      // Добавьте ВСЕ неймспейсы, которые используются на этой странице
-      ...(await serverSideTranslations(locale, [
-        "common", // общие переводы (обязательно!)
-        "header", // для хедера
-        "home", // если у вас есть home.json
-        "rent", // возможно для секций аренды
-        "sale", // возможно для секций продажи
-        // добавьте другие неймспейсы, которые использует HomePage
-      ])),
+      ...(await serverSideTranslations(
+        locale ?? "ru",
+        ["common", "rent", "sale", "header"],
+        config
+      )),
     },
   };
 }
